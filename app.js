7293eec1d3a49e05904b9f6318ba4f6c4b0a57bd
divineion <najima.guermoudi@gmail.com>
2023-05-25 23:53:46 +0200

    "fix/main : ajout du regex sur le champ de saisie du mois

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const birthYear = document.querySelector(".birthYear");
-const  birthMonth = document.querySelector(".birthMonth");
+const birthMonth = document.querySelector(".birthMonth");
 const birthDay = document.querySelector(".birthDay");
 const age = document.querySelector("#age");
 const button = document.querySelector("button");
@@ -10,10 +10,13 @@ const summary = document.querySelector("#summary");
 const daySummary = document.querySelector(".daySummary");
 const monthSummary = document.querySelector(".monthSummary");
 const yearSummary = document.querySelector(".yearSummary");
-// const form = document.querySelector("form");
 const test = document.querySelector("#test");
 const currentMonth = today.getMonth();
+const ageInMonth = document.querySelector(".monthes");
+const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const ageInDays = document.querySelector(".days");
 
+//vérification de la saisie user
 birthDay.addEventListener("blur", function (e) {
   e.preventDefault;
   if (birthDay.value < 01 || birthDay.value > 31) {
@@ -26,7 +29,7 @@ birthDay.addEventListener("blur", function (e) {
   }
 });
 
-birthMonth.addEventListener("blur", function (e) {
+birthMonth.addEventListener("blur", function () {
   const dayMonthTally = function () {
     birthMonth.classList.remove("invalid");
     birthDay.classList.remove("invalid");
@@ -39,38 +42,17 @@ birthMonth.addEventListener("blur", function (e) {
     errorMessage.classList.add("invalid");
     errorMessage.textContent = "This month seems to be longer than usual.";
   };
-  switch (birthMonth.value) {
-    case "02":
-      if (birthDay.value > 29) {
-        dayMonthTallyFailure();
-      } else {
-        dayMonthTally();
-      }
-      break;
-    case "04":
-    case "06":
-    case "09":
-    case "11":
-      if (birthDay.value > 30) {
-        dayMonthTallyFailure();
-      } else {
-        dayMonthTally();
-      }
-      break;
-    case "01":
-    case "03":
-    case "05":
-    case "07":
-    case "08":
-    case "10":
-    case "12":
+
+  for (i = 0; i < birthMonth.value; i++)
+    if (birthDay.value > daysInMonth[i]) {
+      console.log("error");
+      dayMonthTallyFailure();
+    } else {
       dayMonthTally();
-      break;
-    default:
-      birthMonth.classList.add("invalid");
-      errorMessage.classList.add("invalid");
-      birthMonth.focus();
-  }
+    }
+
+  const regexMonth = RegExp(/0[1-9]|1[012]/);
+  regexMonth.exec(birthMonth.value) ? dayMonthTally() : dayMonthTallyFailure();
 });
 
 birthYear.addEventListener("blur", function (e) {
@@ -79,38 +61,46 @@ birthYear.addEventListener("blur", function (e) {
     birthYear.classList.add("invalid");
     errorMessage.classList.add("invalid");
     errorMessage.textContent = "You aren't born yet. Please try later.";
+  } else if (
+    birthYear.value == currentYear &&
+    birthMonth.value >= currentMonth
+  ) {
+    birthYear.classList.add("invalid");
+    errorMessage.classList.add("invalid");
+    errorMessage.textContent = "You aren't born yet. Please try later.";
   } else {
     birthYear.classList.remove("invalid");
     errorMessage.classList.remove("invalid");
     button.disabled = false;
   }
-});
+}); //prévoir de supprimer le report de la date de naissance en cas d'erreur
 
+//calcul des diff selon les cas
 test.addEventListener("click", function (e) {
   e.preventDefault;
   daySummary.textContent = birthDay.value;
   monthSummary.textContent = birthMonth.value;
   yearSummary.textContent = birthYear.value;
-  const date2 = new Date(birthYear.value, birthMonth.value-1, birthDay.value);
+  const date2 = new Date(birthYear.value, birthMonth.value - 1, birthDay.value);
   console.log(date2);
   const date1 = today;
   let diffYears = date1.getFullYear() - date2.getFullYear();
-  console.log(diffYears+" ans");
-  if (birthMonth.value>date1.getMonth()) {
-    const date1 = today;
-    console.log('je suis supérieur');
-    console.log(parseInt(birthMonth.value) - (today.getMonth()+1));
-    
-  } 
+  const ageInYears = document.querySelector(".years");
+  ageInYears.textContent = diffYears;
+
+  if (birthDay.value <= today.getDate()) {
+    ageInDays.textContent = today.getDate() - parseInt(birthDay.value);
+  } else if (birthDay.value > today.getDate()) {
+    ageInDays.textContent =
+      daysInMonth[today.getMonth() + 1] - (birthDay.value - today.getDate());
+  }
+  if (birthMonth.value <= today.getMonth() + 1) {
+    ageInMonth.textContent = today.getMonth() + 1 - parseInt(birthMonth.value);
+  } else if (birthMonth.value > today.getMonth() + 1) {
+    console.log(ageInMonth);
+    console.log("mois supérieur");
+    ageInYears.textContent = diffYears - 1;
+    ageInMonth.textContent =
+      12 - (parseInt(birthMonth.value) - (today.getMonth() + 1));
+  }
 });
-
-
-
-// console.log(year);
-// year = year.toString().substring(2);
-// year = parseInt(year);
-
-// contrainte entre année A et A+3
-//expYear.addEventListener('blur', function() {
-//    expYear.value >= year && expYear.value <= (year + 3) ? invalidDate.style.display = "none" : invalidDate.style.display = "block";
-//})
